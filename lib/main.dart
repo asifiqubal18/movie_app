682b9c26ba3a9f35288c3ac9e8cb3dce0fdf5bb8
Asif Iqubal <asif18543>
2021-06-27 08:55:36 +0530

    Sets the theme to dark

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      
     );
   }
 }
