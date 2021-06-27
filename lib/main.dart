3d215ed6ab50fc0853ed555630e95013532554d2
Asif Iqubal <asif18543>
2021-06-27 09:06:49 +0530

    AppBar done

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Home(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.dark),
-      
+      theme: ThemeData(brightness: Brightness.dark,
+      primaryColor: Colors.green),
+
+    );
+  }
+}
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Movies'),
+      centerTitle: true,),
     );
   }
 }
